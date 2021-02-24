b9044173e9200ab5643fe9e1e0e8e4077c16dda6
이정명(LEE JEONGMYUNG) FW <2074074@hynixad.com>
2021-02-24 17:29:05 +0900

    data load update

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ import sys
 import importlib
 
 import numpy as np
+import scipy.io
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print(os.path.abspath(__file__))
@@ -22,24 +23,28 @@ def main():
     print(model)
 
     # load Dataset
+    x = scipy.io.loadmat("../3DShapeNets/volumetric_data/car/30/test/car_000000353_9.mat")
+    m = nn.ConstantPad3d(1,0)
+    x_tensor = torch.tensor(x['instance']) 
+    x_pad = m(x_tensor)
+    #print(x_pad)
 
 
     # load Weights
-    
     model_params = model.state_dict()
     data_dict = np.load("../shapenet10_weights.npz")
     model_params['body.conv1.weight'] = data_dict['conv1.W']
     model_params['body.conv1.bias'] = data_dict['conv1.b']
-    model_params['body.conv1.weight'] = data_dict['conv1.W']
-    model_params['body.conv1.bias'] = data_dict['conv1.b']
-    for key, value in data_dict.items():
-        print(key)
-    for key, value in model_params.items():
-        print(key)
-
-    #model_params["body"] = torch.from_numpy(data_dict["conv1"]).type(float)
+    model_params['body.conv2.weight'] = data_dict['conv2.W']
+    model_params['body.conv2.bias'] = data_dict['conv2.b']
+    model_params['head.fc1.weight'] = data_dict['fc1.W']
+    model_params['head.fc1.bias'] = data_dict['fc1.b']
+    model_params['head.fc2.weight'] = data_dict['fc2.W']
+    model_params['head.fc2.bias'] = data_dict['fc2.b']
     
-    #print(model_params["conv1.weight"])
+    # inference
+    model.eval()
+    y=model.forward(x_pad)
     
 
 
