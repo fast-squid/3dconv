6e876dc6aff005975d5b3fa10832521b83a36b28
fast-squid <dlwjdaud1@gmail.com>
2021-03-19 00:10:16 +0900

    bug fixed

--- kernel/src/model.cu
+++ kernel/src/model.cu
@@ -1,16 +1,36 @@
 #include "model.h"
 
+#define ERROR_CHECK \
+{\
+	cudaError err = cudaGetLastError(); \
+	if ( cudaSuccess != err ) \
+	{\
+		printf("[%s:%d]CUDA ERROR : %s\n", __FILE__, __LINE__, cudaGetErrorString(err) ); \
+	}\
+}
 
-void Model::set_cuda()
+void Model::set_cuda(bool transpose)
 {	
-	Mat temp(1,1,1,1,1);
-	forward(temp);
-	/*
-	set_transpose(filter);
-	int size = filter.n*filter.c*filter.d*filter.h*filter.w;
-	cudaMalloc((void**)&filter.data_dev, sizeof(float)*size);
-	cudaMemcpy(filter.data_dev, filter.data_trans, sizeof(float)*size, cudaMemcpyHostToDevice);
-	printf("filter size %d\n",size);
-	*/
+	Mat* filter_ptr = 0;
+	for(int i=0;i < inner_layers.size();i++)
+	{
+		if(inner_layers[i]->type == CONV)
+		{
+			filter_ptr = &((Conv*)inner_layers[i])->filter;
+			int size = filter_ptr->get_mat_size();
+			float* data_host;
+			if(transpose)
+			{
+				filter_ptr->set_transpose();
+				data_host = filter_ptr->data_trans;
+			}
+			else
+			{
+				data_host = filter_ptr->data;
+			}
+			cudaMalloc((void**)&filter_ptr->data_dev, sizeof(float)*size);
+			cudaMemcpy(filter_ptr->data_dev, data_host, sizeof(float)*size, cudaMemcpyHostToDevice); 
+		}
+	}
 }
 
